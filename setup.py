259e41a934723392cae78130eeb56df370ee9d56
flipflopbboi <flipflopbboideep@gmail.com>
2021-06-20 10:59:35 +0100

    add `setup.py`

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='faceshifter',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/flipflopbboi/faceshifter',
+    license='3-clause BSD license',
+    author='flipflopbboi',
+    author_email='flipflopbboideep@gmail.com',
+    description='Forked from mindslab-ai/faceshifter'
+)
